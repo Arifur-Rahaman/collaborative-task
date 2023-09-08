28b4df3751361cf3351a94ae6ba44a83a09df1ff
Arifur-Rahaman <arifur.rahaman.siflu@gmail.com>
2023-09-08 20:24:32 +0600

    logout functionality implemented

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,13 +1,26 @@
-import { Card } from "antd";
+import { Button, Card, message } from "antd";
 import { FC, ReactNode} from "react";
+import useLocalStore from "../hooks/useLocalStore";
+import { AUTH_DATA } from "../const";
+import { useNavigate } from "react-router-dom";
 interface Props {
     children: ReactNode;
 }
 const Layout: FC<Props> = ({ children}) => {
+    const navigate = useNavigate()
+    const [authData, setAuthData] = useLocalStore(AUTH_DATA, {})
+    const handleLogout = ()=>{
+        setAuthData({})
+        message.success('Logged out successfully!')
+        navigate('/login')
+    }
     return (
         <div>
             <Card className="mb-4">
-                Header
+                <div className="flex justify-between items-center">
+                    <p className="text-lg">{authData?.firstName} {authData.lastName}</p>
+                    <Button onClick={handleLogout}>Logout</Button>
+                </div>
             </Card>
             {children}
         </div>
